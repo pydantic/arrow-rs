6e5812372fd7463ec1278c74d9aa3a7c604e7eb0
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2025-07-06 19:12:39 -0400

    Add profile

--- parquet-variant/perf/large.rs
+++ parquet-variant/perf/large.rs
@@ -1,24 +1,3 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
-extern crate parquet_variant;
-
-use criterion::*;
-
 use parquet_variant::{json_to_variant, Variant, VariantBuilder};
 
 fn generate_large_object() -> (Vec<u8>, Vec<u8>) {
@@ -56,17 +35,7 @@ fn generate_large_object() -> (Vec<u8>, Vec<u8>) {
     variant_builder.finish()
 }
 
-// Generates a large object and performs full validation
-fn bench_validate_large_object(c: &mut Criterion) {
-    c.bench_function("bench_validate_large_object", |b| {
-        let (m, v) = generate_large_object();
-
-        b.iter(|| {
-            std::hint::black_box(Variant::try_new(&m, &v).unwrap());
-        })
-    });
+fn main() {
+    let (m, v) = generate_large_object();
+    std::hint::black_box(Variant::try_new(&m, &v).unwrap());
 }
-
-criterion_group!(benches, bench_validate_large_object);
-
-criterion_main!(benches);
